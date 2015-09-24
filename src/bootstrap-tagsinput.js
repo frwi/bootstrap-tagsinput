c1605190ec58bac31be99e2bd33de75f0dd51873
frwi <fmiramand@gmail.com>
2015-09-24 17:36:57 +0200

    Add param for change delimiter
    delimiter: ',' -> default value

--- src/bootstrap-tagsinput.js
+++ src/bootstrap-tagsinput.js
@@ -23,7 +23,8 @@
       $tag.hide().fadeIn();
     },
     trimValue: false,
-    allowDuplicates: false
+    allowDuplicates: false,
+    delimiter: ','
   };
 
   /**
@@ -84,7 +85,7 @@
         self.remove(self.itemsArray[0]);
 
       if (typeof item === "string" && this.$element[0].tagName === 'INPUT') {
-        var items = item.split(',');
+        var items = item.split(self.options.delimiter);
         if (items.length > 1) {
           for (var i = 0; i < items.length; i++) {
             this.add(items[i], true);
@@ -245,7 +246,7 @@
       var self = this,
           val = $.map(self.items(), function(item) {
             return self.options.itemValue(item).toString();
-          });
+          }).join(self.options.delimiter);
 
       self.$element.val(val, true).trigger('change');
     },
